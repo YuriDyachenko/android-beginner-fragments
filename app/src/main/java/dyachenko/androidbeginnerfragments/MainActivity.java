39846ec6f194b1ec08dfd6ba503ee4945599fb24
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-05-12 10:09:19 +0300

    задание 1: класс заметки Note, ArrayList с заметками - заполнение из XML-ресурса в onCreate MainActivity

--- app/src/main/java/dyachenko/androidbeginnerfragments/MainActivity.java
+++ app/src/main/java/dyachenko/androidbeginnerfragments/MainActivity.java
@@ -4,11 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final List<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fillNotes();
+    }
+
+    private void fillNotes() {
+        XmlPullParser parser = getResources().getXml(R.xml.notes);
+        Note note = null;
+        boolean inEntry = false;
+        String textValue = "";
+
+        try {
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = parser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if ("note".equalsIgnoreCase(tagName)) {
+                            inEntry = true;
+                            note = new Note();
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        textValue = parser.getText();
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if (inEntry) {
+                            if ("note".equalsIgnoreCase(tagName)) {
+                                notes.add(note);
+                                inEntry = false;
+                            } else if ("title".equalsIgnoreCase(tagName)) {
+                                note.setTitle(textValue);
+                            } else if ("body".equalsIgnoreCase(tagName)) {
+                                note.setBody(textValue);
+                            } else if ("created".equalsIgnoreCase(tagName)) {
+                                note.setCreated(textValue);
+                            }
+                        }
+                        break;
+                    default:
+                }
+                eventType = parser.next();
+            }
+        } catch (XmlPullParserException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/dyachenko/androidbeginnerfragments/Note.java
@@ -0,0 +1,59 @@
+package dyachenko.androidbeginnerfragments;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Note {
+    private String title;
+    private String body;
+    private Date created;
+
+    public Note() {
+    }
+
+    public Note(String title, String body, Date created) {
+        this.title = title;
+        this.body = body;
+        this.created = created;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        String[] strings = body.split("\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String str: strings) {
+            stringBuilder.append(str.trim())
+                    .append("\n");
+        }
+        this.body = stringBuilder.toString().trim();
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setCreated(String stringDate) {
+        try {
+            this.created = new SimpleDateFormat("yyyy-MM-dd", Locale.US).parse(stringDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
